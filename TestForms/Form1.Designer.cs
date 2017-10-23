118318b695d0d50908f561a84ec161781e1c1a3b
nansheng2 <270949689@qq.com>
2017-10-23 23:19:02 +0800

    添加测试程序

---
+++ TestForms/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace TestForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtUrl = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cbIdentificationType = new System.Windows.Forms.ComboBox();
+            this.txtIdentificationNumber = new System.Windows.Forms.TextBox();
+            this.btnGetUser = new System.Windows.Forms.Button();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "请求地址";
+            // 
+            // txtUrl
+            // 
+            this.txtUrl.Location = new System.Drawing.Point(72, 20);
+            this.txtUrl.Name = "txtUrl";
+            this.txtUrl.Size = new System.Drawing.Size(282, 21);
+            this.txtUrl.TabIndex = 1;
+            this.txtUrl.Text = "http://localhost:1851/UIManage/User";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "证件类型";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 89);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 12);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "证件号码";
+            // 
+            // cbIdentificationType
+            // 
+            this.cbIdentificationType.FormattingEnabled = true;
+            this.cbIdentificationType.Location = new System.Drawing.Point(72, 50);
+            this.cbIdentificationType.Name = "cbIdentificationType";
+            this.cbIdentificationType.Size = new System.Drawing.Size(282, 20);
+            this.cbIdentificationType.TabIndex = 4;
+            // 
+            // txtIdentificationNumber
+            // 
+            this.txtIdentificationNumber.Location = new System.Drawing.Point(72, 83);
+            this.txtIdentificationNumber.Name = "txtIdentificationNumber";
+            this.txtIdentificationNumber.Size = new System.Drawing.Size(282, 21);
+            this.txtIdentificationNumber.TabIndex = 5;
+            // 
+            // btnGetUser
+            // 
+            this.btnGetUser.Location = new System.Drawing.Point(72, 123);
+            this.btnGetUser.Name = "btnGetUser";
+            this.btnGetUser.Size = new System.Drawing.Size(75, 23);
+            this.btnGetUser.TabIndex = 6;
+            this.btnGetUser.Text = "获取患者信息";
+            this.btnGetUser.UseVisualStyleBackColor = true;
+            this.btnGetUser.Click += new System.EventHandler(this.btnGetUser_Click);
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(15, 168);
+            this.txtResult.Multiline = true;
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(403, 178);
+            this.txtResult.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(430, 358);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.btnGetUser);
+            this.Controls.Add(this.txtIdentificationNumber);
+            this.Controls.Add(this.cbIdentificationType);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtUrl);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "接口测试程序";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtUrl;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cbIdentificationType;
+        private System.Windows.Forms.TextBox txtIdentificationNumber;
+        private System.Windows.Forms.Button btnGetUser;
+        private System.Windows.Forms.TextBox txtResult;
+    }
+}
+
---
+++ TestForms/Form1.cs
@@ -0,0 +1,205 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestForms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            IList<IdentificationType> infoList = new List<IdentificationType>();
+            IdentificationType identificationType = new IdentificationType();
+            identificationType.Key = 1;
+            identificationType.Value = "护照";
+            infoList.Add(identificationType);
+            identificationType = new IdentificationType();
+            identificationType.Key = 2;
+            identificationType.Value = "身体证";
+            infoList.Add(identificationType);
+            cbIdentificationType.DataSource = infoList;
+            cbIdentificationType.ValueMember = "Key";
+            cbIdentificationType.DisplayMember = "Value";
+        }
+
+        private void btnGetUser_Click(object sender, EventArgs e)
+        {
+            string url = txtUrl.Text.Trim();
+            RestClient client = new RestClient(url);
+
+            string method = "GetUserInfo";
+            GetUserInfoParameter param = new GetUserInfoParameter();
+            param.IdentificationType =(int) cbIdentificationType.SelectedValue;
+            param.IdentificationNumber = txtIdentificationNumber.Text.Trim();
+            string json = JsonConvert.SerializeObject(param) ;
+            string retPost = client.Post(json, method);
+            txtResult.Text = retPost;
+        }
+    }
+
+    public class IdentificationType
+    {
+        public int Key
+        {
+            get;
+            set;
+        }
+
+        public string Value
+        {
+            get;
+            set;
+        }
+    }
+
+
+    /// <summary>
+    /// 获取用户信息参数
+    /// </summary>
+    public class GetUserInfoParameter
+    {
+        /// <summary>
+        /// 证件类型
+        /// </summary>
+        public int IdentificationType
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// 证件号码
+        /// </summary>
+        public string IdentificationNumber
+        {
+            get;
+            set;
+        }
+    }
+
+    public class Response<T>
+    {
+
+        /// <summary>
+        /// 是否成功
+        /// </summary>
+        public bool IsSuccessfull
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// 失败为原因
+        /// </summary>
+        public string Reason
+        {
+            get;
+            set;
+        }
+
+
+        /// <summary>
+        /// 数据列表
+        /// </summary>
+        public T Result
+        {
+            get;
+            set;
+        }
+
+
+        /// <summary>
+        /// 当前查询总条数
+        /// </summary>
+        public int TotalCount
+        {
+            get;
+            set;
+        }
+    }
+
+    public class GetUserInfoResult
+    {
+        /// <summary>
+        /// 证件类型
+        /// 1：护照
+        /// 2：身体证
+        /// </summary>
+        public int IdentificationType
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// 证件号码
+        /// </summary>
+        public string IdentificationNumber
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// 患者姓名
+        /// </summary>
+        public string Name
+        {
+            get;
+            set;
+        }
+
+
+        /// <summary>
+        /// 电话
+        /// </summary>
+        public string Tel
+        {
+            get;
+            set;
+        }
+
+
+        /// <summary>
+        /// 性别
+        /// 1:男
+        /// 2:女
+        /// </summary>
+        public int Gender
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// 出生年月(yyyy-MM-dd)
+        /// </summary>
+        public DateTime Birthday
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// 就诊卡号
+        /// </summary>
+        public string CardNumber
+        {
+            get;
+            set;
+        }
+    }
+}
