d273c6235f90477193c1d3079d4448911966ac0d
nansheng2 <270949689@qq.com>
2017-10-24 23:53:43 +0800

    提交所有源代码

---
+++ NFine.Web.Config/Form1.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace NFine.Web.Config
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtDBServer = new System.Windows.Forms.TextBox();
+            this.txtDBName = new System.Windows.Forms.TextBox();
+            this.txtUserName = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.txtHisUrl = new System.Windows.Forms.TextBox();
+            this.tbnSave = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtOrderCycle = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.cbIsTest = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "服务器名称";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "数据库名称";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(48, 80);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 12);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "用户名";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(60, 107);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 12);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "密码";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(18, 134);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(71, 12);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "His接口地址";
+            // 
+            // txtDBServer
+            // 
+            this.txtDBServer.Location = new System.Drawing.Point(96, 24);
+            this.txtDBServer.Name = "txtDBServer";
+            this.txtDBServer.Size = new System.Drawing.Size(307, 21);
+            this.txtDBServer.TabIndex = 5;
+            this.txtDBServer.Text = ".";
+            // 
+            // txtDBName
+            // 
+            this.txtDBName.Location = new System.Drawing.Point(95, 51);
+            this.txtDBName.Name = "txtDBName";
+            this.txtDBName.Size = new System.Drawing.Size(307, 21);
+            this.txtDBName.TabIndex = 6;
+            this.txtDBName.Text = "HuiBin";
+            // 
+            // txtUserName
+            // 
+            this.txtUserName.Location = new System.Drawing.Point(96, 77);
+            this.txtUserName.Name = "txtUserName";
+            this.txtUserName.Size = new System.Drawing.Size(307, 21);
+            this.txtUserName.TabIndex = 7;
+            this.txtUserName.Text = "sa";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(95, 104);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(307, 21);
+            this.txtPassword.TabIndex = 8;
+            this.txtPassword.Text = "123";
+            // 
+            // txtHisUrl
+            // 
+            this.txtHisUrl.Location = new System.Drawing.Point(96, 131);
+            this.txtHisUrl.Name = "txtHisUrl";
+            this.txtHisUrl.Size = new System.Drawing.Size(307, 21);
+            this.txtHisUrl.TabIndex = 9;
+            this.txtHisUrl.Text = "http://localhost:1851/";
+            // 
+            // tbnSave
+            // 
+            this.tbnSave.Location = new System.Drawing.Point(96, 223);
+            this.tbnSave.Name = "tbnSave";
+            this.tbnSave.Size = new System.Drawing.Size(75, 23);
+            this.tbnSave.TabIndex = 10;
+            this.tbnSave.Text = "保存";
+            this.tbnSave.UseVisualStyleBackColor = true;
+            this.tbnSave.Click += new System.EventHandler(this.tbnSave_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(18, 164);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(77, 12);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "预约周期[天]";
+            // 
+            // txtOrderCycle
+            // 
+            this.txtOrderCycle.Location = new System.Drawing.Point(95, 161);
+            this.txtOrderCycle.Name = "txtOrderCycle";
+            this.txtOrderCycle.Size = new System.Drawing.Size(307, 21);
+            this.txtOrderCycle.TabIndex = 12;
+            this.txtOrderCycle.Text = "8";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(36, 197);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(53, 12);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "是否测试";
+            // 
+            // cbIsTest
+            // 
+            this.cbIsTest.FormattingEnabled = true;
+            this.cbIsTest.Location = new System.Drawing.Point(96, 192);
+            this.cbIsTest.Name = "cbIsTest";
+            this.cbIsTest.Size = new System.Drawing.Size(306, 20);
+            this.cbIsTest.TabIndex = 14;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(419, 268);
+            this.Controls.Add(this.cbIsTest);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtOrderCycle);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.tbnSave);
+            this.Controls.Add(this.txtHisUrl);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtUserName);
+            this.Controls.Add(this.txtDBName);
+            this.Controls.Add(this.txtDBServer);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "网站配置";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtDBServer;
+        private System.Windows.Forms.TextBox txtDBName;
+        private System.Windows.Forms.TextBox txtUserName;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.TextBox txtHisUrl;
+        private System.Windows.Forms.Button tbnSave;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtOrderCycle;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ComboBox cbIsTest;
+    }
+}
+
---
+++ NFine.Web.Config/Form1.cs
@@ -0,0 +1,249 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace NFine.Web.Config
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        #region 保存
+        /// <summary>
+        /// 保存 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void tbnSave_Click(object sender, EventArgs e)
+        {
+            bool isReturn = false;
+
+            var server = this.txtDBServer.Text.Trim();
+            var dbName = this.txtDBName.Text.Trim();
+            var dbUser = this.txtUserName.Text.Trim();
+            var dbPassword = this.txtPassword.Text.Trim();
+            var hisUrl = this.txtHisUrl.Text.Trim();
+            var orderCycle = this.txtOrderCycle.Text.Trim();
+            var isTest = this.cbIsTest.SelectedValue.ToString();
+            if (string.IsNullOrEmpty(server))
+            {
+                isReturn = true;
+            }
+            if (string.IsNullOrEmpty(dbName))
+            {
+                isReturn = true;
+            }
+            if (string.IsNullOrEmpty(dbUser))
+            {
+                isReturn = true;
+            }
+
+            if (string.IsNullOrEmpty(dbPassword))
+            {
+                isReturn = true;
+            }
+
+            if (string.IsNullOrEmpty(hisUrl))
+            {
+                isReturn = true;
+            }
+
+            if (string.IsNullOrEmpty(orderCycle))
+            {
+                isReturn = true;
+            }
+            if (isReturn)
+            {
+                MessageBox.Show("输入不能为空");
+                return;
+            }
+            //获取当前程序根目录
+            var rootPath = System.Environment.CurrentDirectory;
+            var appFile = rootPath + "/Configs/database.config";
+
+            try
+            {
+                //如果找到Web.config
+                if (File.Exists(appFile))
+                {
+                    XmlDocument configDoc = new XmlDocument();
+                    configDoc.Load(appFile);
+
+                    XmlNode appSettingsNode = configDoc.SelectSingleNode("connectionStrings");
+
+                    //遍历所有子节点，找到ServiceIPAddress节点并返回
+                    foreach (XmlNode node in appSettingsNode.ChildNodes)
+                    {
+                        if (node.NodeType != XmlNodeType.Element)
+                        {
+                            continue;
+                        }
+
+                        XmlElement xele = (XmlElement)node;
+                        var keyToken = xele.GetAttribute("name");
+
+                        var tempConn = string.Format("Server={0};Initial Catalog={1};User Id={2};Password={3};MultipleActiveResultSets=true;Max Pool Size=150", server, dbName, dbUser, dbPassword);
+                        try
+                        {
+
+                            using (SqlConnection connection = new SqlConnection(tempConn))
+                            {
+
+                                connection.Open();
+
+                                if (connection.State == ConnectionState.Open)
+                                {
+
+                                    MessageBox.Show("连接成功！");
+
+                                }
+                                else
+                                {
+                                    MessageBox.Show("连接失败！");
+                                    return;
+                                }
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show(ex.Message);
+                            return;
+                        }
+
+                        switch (keyToken)
+                        {
+                            case "NFineDbContext":
+                                xele.SetAttribute("connectionString", tempConn);
+                                break;
+                        }
+                    }
+
+                    configDoc.Save(appFile);
+
+                    //修改其它配置
+                    appFile = rootPath + "/Configs/system.config";
+                    //如果找到Web.config
+                    if (File.Exists(appFile))
+                    {
+                        configDoc = new XmlDocument();
+                        configDoc.Load(appFile);
+
+                        appSettingsNode = configDoc.SelectSingleNode("appSettings");
+
+                        //遍历所有子节点，找到ServiceIPAddress节点并返回
+                        foreach (XmlNode node in appSettingsNode.ChildNodes)
+                        {
+                            if (node.NodeType != XmlNodeType.Element)
+                            {
+                                continue;
+                            }
+
+                            XmlElement xele = (XmlElement)node;
+                            var keyToken = xele.GetAttribute("key");
+
+                            switch (keyToken)
+                            {
+                                case "OrderCycle":
+                                    xele.SetAttribute("value", orderCycle);
+                                    break;
+                                case "HisUrl":
+                                    xele.SetAttribute("value", hisUrl);
+                                    break;
+                                case "IsTest":
+                                    xele.SetAttribute("value", isTest);
+                                    break;
+                            }
+                        }
+
+                        configDoc.Save(appFile);
+
+                        //修改其它配置
+                        MessageBox.Show("配置文件保存成功！");
+
+                        Application.Exit();
+                    }
+                    else
+                    {
+                        MessageBox.Show("database.config 配置文件未找到！");
+                    }
+
+                    MessageBox.Show("配置文件保存成功！");
+
+                    Application.Exit();
+                }
+                else
+                {
+                    MessageBox.Show("database.config 配置文件未找到！");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        #endregion
+
+        #region 加载界面
+        /// <summary>
+        /// 加载界面
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            IList<Select> infoList = new List<Select>();
+            Select select = new Select();
+            select.Key = 1;
+            select.Value = "是";
+            infoList.Add(select);
+            select = new Select();
+            select.Key = 0;
+            select.Value = "否";
+            infoList.Add(select);
+            cbIsTest.DataSource = infoList;
+            cbIsTest.ValueMember = "Key";
+            cbIsTest.DisplayMember = "Value";
+        }
+        #endregion
+
+    }
+
+    #region 选择
+    /// <summary>
+    /// 选择
+    /// </summary>
+    public class Select
+    {
+        /// <summary>
+        /// Key
+        /// </summary>
+        public int Key
+        {
+            set;
+            get;
+        }
+
+        /// <summary>
+        /// 值
+        /// </summary>
+        public string Value
+        {
+            get;
+            set;
+        }
+    }
+    #endregion
+
+}
